d14b4c4d4c762f39c3d5b5d4d6746ec83ce1cd9d
iBug <git@ibugone.com>
2020-05-26 14:30:45 +0800

    Add application-helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def title(text)
+    content_for :title, text
+    text
+  end
+
+  def current_action?(cls, action = nil)
+    return false unless controller.is_a? cls
+    if action
+      controller.action_name == action.to_s
+    else
+      true
+    end
+  end
 end
