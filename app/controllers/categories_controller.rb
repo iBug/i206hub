fef2b2261c96f8835e15401c2bdb773af841393c
iBug <git@ibugone.com>
2020-06-09 20:24:36 +0800

    Category index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-  layout 'card', only: [:index]
+  layout 'card', only: %i[index show]
 
   # GET /
   def home
