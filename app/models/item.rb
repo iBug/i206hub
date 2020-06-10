a3c6682a2c3ab99c0b363d4f7a3fe72b744765ac
iBug <git@ibugone.com>
2020-06-10 19:42:42 +0800

    Add custom HTML sanitizer class
    XSS ready~

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
+class PostScrubber < Rails::Html::PermitScrubber
+  def initialize
+    super
+    self.tags = %w[
+      a b blockquote br code del details dd div dl dt em h1 h2 h3 h4 h5 h6 hr i img kbd
+      li mark ol p pre s sp span sup sub strong strike summary ul
+    ]
+    self.attributes = %w[href title src height width alt]
+  end
+
+  def skip_node?(node)
+    node.text?
+  end
+end
+
 class Item < ApplicationRecord
   belongs_to :category
   has_many :comments
