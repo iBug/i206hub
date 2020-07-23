fe5c3d1843df3df57f3d278ce6c811dd7faf4940
iBug <git@ibugone.com>
2020-07-23 10:47:54 +0800

    Remove listen gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Load Git Revision dynamically
   config.define_singleton_method :git_revision do
